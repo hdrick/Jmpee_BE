317452dc4dcd8eebc547e3416f8290dbc059ad0c
hendwrick <hendwrick.gonzales@ext.essilor.ph>
2024-08-20 22:29:57 +0800

    initial commit for

---
+++ src/test/java/com/hdrick/jumpee/JumpeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdrick.jumpee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JumpeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
